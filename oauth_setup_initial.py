58c3bfc5b085750deb246586afa72a7527a5a4f1
jvaleo <jvaleo@mac.com>
2013-12-01 14:55:23 -0500

    Cleaned up imports

--- oauth_setup_initial.py
+++ oauth_setup_initial.py
@@ -1,31 +1,10 @@
 from __future__ import unicode_literals
 from requests_oauthlib import OAuth1
 from urlparse import parse_qs
-import simplejson
-import json
-import requests
-from twython import Twython
-import subprocess
-import string
-import random
-import time
-import os
-from time import sleep, strftime
-from subprocess import *
-from subprocess import call
-import imaplib
-import smtplib
-import email
-from email.mime.text import MIMEText
-from email.parser import HeaderParser
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEBase import MIMEBase
-from email.Utils import COMMASPACE, formatdate
-from email import Encoders
-#########################################################################
-from email_cred import *
+
 from keys import * #import APP_KEY, APP_SECRET,  OAUTH_TOKEN, OAUTH_TOKEN_SECRET
 from urls import * #import REQUEST_TOKEN_URL, AUTHORIZE_URL, ACCESS_TOKEN_URL
+
 def setup_oauth():
     """Authorize your app via identifier."""
     # Request token
